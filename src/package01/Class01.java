99fed87968fc75690cd1d10057ce61684a6cb4cc
Vishal Simgh <vishalsingh164@gmail.com>
2023-09-02 19:40:48 +0530

    This is commit 01 by User 01 in branch develop

--- src/package01/Class01.java
+++ src/package01/Class01.java
@@ -11,6 +11,8 @@ public class Class01 {
 		System.out.println("Hello this is added by user 01");
 
 		System.out.println("This line is added by user 02" );
+		
+		System.out.println("Some changes done in branch develop");
 
 	}
 
